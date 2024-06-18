10387cdf4ddebcf96ca6247dd9c19f436d5a61d2
Micael Viana <micael.vianasilva@gmail.com>
2024-06-17 20:13:12 -0400

    add main

---
+++ main.py
@@ -0,0 +1,34 @@
+import sys
+from antlr4 import *
+from MiniCLexer import MiniCLexer
+from MiniCParser import MiniCParser
+
+# Verifica se o usuário forneceu o nome do arquivo como argumento
+if len(sys.argv) != 2:
+    print("Uso: python main.py nome_do_arquivo")
+    sys.exit(1)
+
+# Obtém o nome do arquivo a partir dos argumentos da linha de comando
+nome_arquivo = sys.argv[1]
+
+# Lê o conteúdo do arquivo
+try:
+    with open(nome_arquivo, 'r') as file:
+        data = file.read()
+except FileNotFoundError:
+    print(f"Arquivo '{nome_arquivo}' não encontrado.")
+    sys.exit(1)
+
+# Cria um InputStream a partir dos dados lidos do arquivo
+input_stream = InputStream(data)
+
+# Inicializa o lexer e o parser
+lexer = MiniCLexer(input_stream)
+token_stream = CommonTokenStream(lexer)
+parser = MiniCParser(token_stream)
+
+# Realiza a análise sintática
+tree = parser.program()
+
+# Imprime a árvore de análise
+print(tree.toStringTree(recog=parser))
